afca8fb5bf9e8005d371cd2f1bb28b15d9c40070
Hasan Emre Tekce <hasan-emre.tekce@stud.hs-hannover.de>
2020-09-30 21:20:57 +0200

    alle seite ist auch syncroniziert mit linken

--- herren.php
+++ herren.php
@@ -10,7 +10,7 @@ $stmt = $pdo->query("SELECT `Produkt_ID`, `Produkt_Name`,`bilder_path` FROM `pro
 while ($row = $stmt->fetch()) {
 	echo     "<div class='col-lg-3 col-md-4 col-6'><a href='einzelprodukt.php?edit=".$row['Produkt_ID']."' class='d-block mb-4 h-100'>";
 
-      echo      "<img class='img-fluid img-thumbnail' src='".$row['bilder_path']."' alt=''></a>  </div>";
+      echo      "<img class='img-fluid img-thumbnail' src='".$row['bilder_path']."' alt=''>" .$row['Produkt_Name']." </a>  </div>";
 
 }
 	
